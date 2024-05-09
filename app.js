d4b9d8663ec0dc307e5926cd180310558413d631
박형건 <qkrgudrjs1@gmail.com>
2024-05-09 11:39:09 +0900

    Sequelize적용

--- app.js
+++ app.js
@@ -1,24 +1,19 @@
-import express from 'express'
-import morgan from 'morgan'
-import tweetsRouter from './router/tweets.js'
-import authRouter from './router/auth.js'
-import { config } from './config.js'
-import { db } from './db/database.js'
-
-
-const app = express()
-
-app.use(express.json())
-app.use(morgan('dev'))
-
-app.use('/tweets', tweetsRouter)
-app.use('/auth', authRouter)
-
-app.use((req,res,next) => {
-    res.sendStatus(404)
-})
-
+import express from "express";
+import morgan from "morgan";
+import tweetsRouter from './Router/tweets.js';
+import authRouter from './Router/auth.js';
+import { config } from "./config.js";
+import { sequelize } from "./db/database.js";
+const app = express();
+app.use(express.json());
+app.use(morgan("dev"));
+// http://localhost:8080/tweets
+app.use('/tweets', tweetsRouter);
+app.use('/auth',authRouter);
+app.use((req,res, next)=>{
+    res.sendStatus(404);
+});
 // DB 연결 테스트!
-// db.getConnection().then(connection => console.log(connection))
-
-app.listen(config.host.port)
+sequelize.sync().then(()=>{
+    app.listen(config.host.port);
+});
--- data/auth.js
+++ data/auth.js
@@ -1,18 +1,51 @@
-import { db } from '../db/database.js'
+import SQ from 'sequelize';
+import { sequelize } from '../db/database.js';
+const DataTypes = SQ.DataTypes;
+// sequelize에서 사용하는 모든 데이터 형(INT,STRING ...)을 저장하고 생성하게끔 만들어줌.
+//tweets User  쿼리 문을 안쓰기 위해서
+export const User = sequelize.define(
+    'user',
+    {
+        id:{
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            allowNull: false,
+            primaryKey: true
+        },
+        username:{
+            type: DataTypes.STRING(50),
+            allowNull: false
+        },
+        password: {
+            type: DataTypes.STRING(150),
+            allowNull: false
+        },
+        name: {
+            type: DataTypes.STRING(50),
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING(50),
+            allowNull: false
+        },
+        url: DataTypes.STRING(1000)
+    },
+    { timestamps:false}
+);
+// 외부에서 User라는 사용자 틀을 가져다가 사용할 수 있게끔 만들어줌.
 
 // 아이디(username) 중복검사
 export async function findByUsername(username){
-    return db.execute('select * from users where username = ?', [username]).then((result) => result[0][0])
+    return User.findOne({where: {username}})
 }
 
 //id 중복검사
 export async function findById(id){
-    return db.execute('select * from users where id = ?', [id]).then((result) => result[0][0])
+    return User.findByPk(id)
 }
 
 export async function createUser(user){
-    const {username, hashed, name, email, url} =user
-    return db.execute('insert into users(username, password, name, email, url) values(?, ?, ?, ?, ?)', [username, hashed, name, email, url]).then((result) => result[0].insertId)
+    return User.create(user).then((data) => data.dataValues.id)
 }
 
 // export async function login(username){
